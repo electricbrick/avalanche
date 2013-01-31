0799f3cd0da47d0929bc9a6d70c565611cc6d51e
Brian Gardner <bmg1227@gmail.com>
2013-01-30 20:21:01 -0600

    Add styling for eNews widget and change topnav text.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ unregister_sidebar( 'sidebar-alt' );
 // Add the topnav section
 add_action( 'genesis_before', 'bg_topnav' );
 function bg_topnav() {
-	echo '<div id="topnav"><div class="wrap"><span class="left"><span class="from-the-blog">Like this theme?</span><a href="https://github.com/bgardner/avalanche">You can fork it on GitHub.</a></span><span class="right"><a class="first" href="http://www.briangardner.com/about/">The 411 on Me</a><a href="http://www.briangardner.com/code/">Code Snippets</a><a href="http://www.briangardner.com/themes/">WordPress Themes</a></span></div></div>';
+	echo '<div id="topnav"><div class="wrap"><span class="left"><span class="from-the-blog">Like this theme?</span><a href="http://my.studiopress.com/themes/metro/">You can buy it on StudioPress.</a></span><span class="right"><a class="first" href="http://www.briangardner.com/about/">The 411 on Me</a><a href="http://www.briangardner.com/code/">Code Snippets</a><a href="http://www.briangardner.com/themes/">WordPress Themes</a></span></div></div>';
 }
 
 // Customize the post info function
